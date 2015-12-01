f58da550a8ddba099b18f3e7e25d5277fff4b98b
kalkih <kalle_kihlstrom@hotmail.com>
2015-12-01 19:29:17 +0100

    Working viewPager with tabs

---
+++ app/src/main/java/com/bling/app/fragments/HistoryFragment.java
@@ -0,0 +1,29 @@
+package com.bling.app.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bling.app.R;
+
+public class HistoryFragment extends Fragment{
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/bling/app/fragments/NearbyFragment.java
@@ -0,0 +1,29 @@
+package com.bling.app.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bling.app.R;
+
+public class NearbyFragment extends Fragment{
+
+    public NearbyFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nearby, container, false);
+    }
+
+}
