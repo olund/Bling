f57edd6c951b1766a6d80628ac0b2aa425841ee5
kalkih <kalle_kihlstrom@hotmail.com>
2015-12-04 18:25:59 +0100

    Added ButtonListeners with basic intents

--- app/src/main/java/com/bling/app/activity/SplashActivity.java
+++ app/src/main/java/com/bling/app/activity/SplashActivity.java
@@ -1,15 +1,45 @@
 package com.bling.app.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.bling.app.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private Button mRegisterButton;
+    private Button mLoginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        setupButtons();
+    }
+
+    private void setupButtons() {
+        mRegisterButton = (Button) findViewById(R.id.register_button);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+
+        // Adding Listeners
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, RegisterActivity.class);
+
+                startActivity(intent);
+            }
+        });
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
