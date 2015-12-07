e8b0b0669e3111cbed4f144146e5a960bea917bc
kalkih <kalle_kihlstrom@hotmail.com>
2015-12-07 21:42:45 +0100

    Fixed intent for dinstance activity

--- app/src/main/java/com/bling/app/activity/DistanceActivity.java
+++ app/src/main/java/com/bling/app/activity/DistanceActivity.java
@@ -1,12 +1,18 @@
 package com.bling.app.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bling.app.R;
+import com.bling.app.helper.Message;
 
 public class DistanceActivity extends AppCompatActivity {
+
+    private TextView mDistanceText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,11 @@ public class DistanceActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        Message message = (Message)intent.getSerializableExtra("message");
+
+        mDistanceText = (TextView) findViewById(R.id.distance);
+        mDistanceText.setText("test");
     }
 }
