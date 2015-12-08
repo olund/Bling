eeddb793b5b801b11d7cbe7415861113b50df2eb
kalkih <kalle_kihlstrom@hotmail.com>
2015-12-08 21:42:07 +0100

    Added URL constants

--- app/src/main/java/com/bling/app/helper/Constant.java
+++ app/src/main/java/com/bling/app/helper/Constant.java
@@ -5,7 +5,7 @@ package com.bling.app.helper;
  */
 
 public class Constant {
-
+    public static final String URL_BASE = "http://192.168.1.210:3000/";
     public static final String MESSAGE_TYPE_FRIEND_REQUEST = "friendRequest";
     public static final String MESSAGE_TYPE_POSITION = "position";
     public static final String MESSAGE_TYPE_DISTANCE = "distance";
@@ -14,6 +14,8 @@ public class Constant {
     public static final String USERNAME = "username";
     public static final String EMAIL = "email";
     public static final String USER_ID = "userId";
+    public static final String URL_LOGIN = URL_BASE + "users/login";
+    public static final String URL_REGISTER = URL_BASE + "users/register";
 
 
 }
