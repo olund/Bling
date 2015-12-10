d2417b3d33e79ff384f9b0fbabd212cddaf82248
kalkih <kalle_kihlstrom@hotmail.com>
2015-12-10 17:47:34 +0100

    Added send bling on friendlist click

--- app/src/main/java/com/bling/app/fragments/FriendsFragment.java
+++ app/src/main/java/com/bling/app/fragments/FriendsFragment.java
@@ -1,5 +1,6 @@
 package com.bling.app.fragments;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
@@ -8,10 +9,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,6 +25,10 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.bling.app.R;
 import com.bling.app.activity.AddFriendActivity;
+import com.bling.app.activity.DistanceActivity;
+import com.bling.app.activity.FriendRequestActivity;
+import com.bling.app.activity.MainActivity;
+import com.bling.app.activity.PositionActivity;
 import com.bling.app.app.BlingApp;
 import com.bling.app.helper.Constant;
 import com.bling.app.helper.Friend;
@@ -43,6 +51,7 @@ public class FriendsFragment extends Fragment implements SwipeRefreshLayout.OnRe
     private ListView listView;
     private SwipeFriendListAdapter adapter;
     private List<Friend> friendList;
+    private Location mLocation;
     private String mUser;
 
     // initially offset will be 0, later will be updated while parsing the json
@@ -89,6 +98,36 @@ public class FriendsFragment extends Fragment implements SwipeRefreshLayout.OnRe
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                final Friend friend = (Friend) listView.getItemAtPosition(position);
+
+                Log.d(TAG, "Friend: " + friend.username + " clicked.");
+
+                String names[] = {getString(R.string.alternative_distance),getString(R.string.alternative_position)};
+                final AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                View convertView = (View) inflater.inflate(R.layout.alert_list, null);
+                alertDialog.setView(convertView);
+                alertDialog.setTitle(R.string.bling_alternatives);
+                ListView lv = (ListView) convertView.findViewById(R.id.listView1);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, names);
+                lv.setAdapter(adapter);
+                alertDialog.show();
+
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+                        if (pos == 0) {
+                            sendRequest(friend, Constant.MESSAGE_TYPE_DISTANCE);
+                        } else if(pos == 1){
+                            sendRequest(friend, Constant.MESSAGE_TYPE_POSITION);
+                        }
+                        alertDialog.dismiss();
+                    }
+                });
+            }
+        });
         /**
          * Showing Swipe Refresh animation on activity create
          * As animation won't start on onCreate, post runnable is used
@@ -103,10 +142,49 @@ public class FriendsFragment extends Fragment implements SwipeRefreshLayout.OnRe
         return rootView;
     }
 
+    private void sendRequest(Friend friend, String type) {
+        String URL = Constant.URL_MESSAGES;
+
+        // Create JSON object to SEND.
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("fromId", mUser);
+            obj.put("toId", friend.id);
+            obj.put("type", type);
+            obj.put("latitude", mLocation.getLatitude());
+            obj.put("longitude", mLocation.getLongitude());
+            obj.put("read", false);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Send HTTP POST with JSON object.
+        JsonObjectRequest req = new JsonObjectRequest(URL, obj, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.i(TAG, response.toString(4));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "Failed to send Bling.");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.getMessage());
+                Log.e(TAG, "Failed to send Bling.");
+            }
+        });
+
+        // Adding request to request queue
+        BlingApp.getInstance().addToRequestQueue(req);
+    }
+
     @Override
     public void locationChanged() {
-        Location location = LocationModel.getInstance().getLocation();
-        Log.d(TAG, "FriendsFragment says: Location changed: " + String.valueOf(location));
+        mLocation = LocationModel.getInstance().getLocation();
+        Log.d(TAG, "FriendsFragment says: Location changed: " + String.valueOf(mLocation));
     }
 
     /**
